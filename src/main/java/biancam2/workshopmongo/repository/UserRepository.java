a9e39048d3aca4b310dfc17d945972bda6ae3349
BiancaM2 <bianca.morschbacher@gmail.com>
2025-01-20 09:57:55 -0300

    Connecting to Mongodb with repository and service

---
+++ src/main/java/biancam2/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package biancam2.workshopmongo.repository;
+
+import biancam2.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+
+}
