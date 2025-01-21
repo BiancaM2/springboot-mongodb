782abc3b9f0d6c96742e087e7a9a0d24eb25a7dd
BiancaM2 <bianca.morschbacher@gmail.com>
2025-01-21 08:22:31 -0300

    Projection of author's data with DTO

--- src/main/java/biancam2/workshopmongo/config/Instantiation.java
+++ src/main/java/biancam2/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package biancam2.workshopmongo.config;
 
 import biancam2.workshopmongo.domain.Post;
 import biancam2.workshopmongo.domain.User;
+import biancam2.workshopmongo.dto.AuthorDTO;
 import biancam2.workshopmongo.repository.PostRepository;
 import biancam2.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,11 +35,12 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para SP", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!", maria);
-
 
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para SP", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
