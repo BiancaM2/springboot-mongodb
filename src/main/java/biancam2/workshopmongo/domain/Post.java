aabedb2dab0a1e2bc46538d8124e845995f43145
BiancaM2 <bianca.morschbacher@gmail.com>
2025-01-21 14:29:25 -0300

    Getting a post by id

--- src/main/java/biancam2/workshopmongo/domain/Post.java
+++ src/main/java/biancam2/workshopmongo/domain/Post.java
@@ -6,11 +6,10 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.Objects;
 
 @Document
 public class Post implements Serializable {
-    public static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     @Id
     private String id;
@@ -19,9 +18,11 @@ public class Post implements Serializable {
     private String body;
     private AuthorDTO author;
 
-    public Post(){}
+    public Post() {
+    }
 
     public Post(String id, Date date, String title, String body, AuthorDTO author) {
+        super();
         this.id = id;
         this.date = date;
         this.title = title;
@@ -70,14 +71,27 @@ public class Post implements Serializable {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Post post = (Post) o;
-        return Objects.equals(id, post.id);
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Post other = (Post) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
     }
 }
---
+++ src/main/java/biancam2/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package biancam2.workshopmongo.services;
+
+import biancam2.workshopmongo.domain.Post;
+import biancam2.workshopmongo.repository.PostRepository;
+import biancam2.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
+
